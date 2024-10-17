5ee6cf9248b4214117627ae55978e35a491f4485
mstaali <imthetigerz@gmail.com>
2024-10-17 01:34:26 +0100

    norms_frames & textures

--- parsing/frames.c
+++ parsing/frames.c
@@ -6,7 +6,7 @@
 /*   By: mstaali <mstaali@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/01 15:11:26 by mstaali           #+#    #+#             */
-/*   Updated: 2024/10/16 15:43:53 by mstaali          ###   ########.fr       */
+/*   Updated: 2024/10/17 01:33:11 by mstaali          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -43,31 +43,55 @@ void	load_sprite_frames(my_mlx_t *mlx)
 
 	mlx->num_frames = 7;
 	mlx->curr_frame = 0;
-    mlx->is_animated = 0;
-    mlx->last_frame_time = mlx_get_time();
-	mlx->sprite_textures = (mlx_texture_t **)malloc(sizeof(mlx_texture_t *) * mlx->num_frames);
+	mlx->is_animated = 0;
+	mlx->last_frame_time = mlx_get_time();
+	mlx->sprite_textures = (mlx_texture_t **)malloc(sizeof(mlx_texture_t *)
+			* mlx->num_frames);
 	if (!mlx->sprite_textures)
 		error_mssg(FRAMES);
 	fill_frame_paths(mlx->sprite_textures);
-	mlx->sprite_frames = (mlx_image_t **)malloc(sizeof(mlx_image_t *) * mlx->num_frames);
-	//need to delete & free textures
+	mlx->sprite_frames = (mlx_image_t **)malloc(sizeof(mlx_image_t *)
+			* mlx->num_frames);
 	if (!mlx->sprite_frames)
 		error_mssg(FRAMES);
 	i = -1;
 	while (++i < mlx->num_frames)
 	{
-		mlx->sprite_frames[i] = mlx_texture_to_image(mlx->mlx, mlx->sprite_textures[i]);
+		mlx->sprite_frames[i] = mlx_texture_to_image(mlx->mlx,
+				mlx->sprite_textures[i]);
 		if (!mlx->sprite_frames[i])
 			error_mssg(FRAMES);
+	}
+}
+
+void	animation_process(my_mlx_t *mlx, double current_time)
+{
+	current_time = mlx_get_time();
+	if (current_time - mlx->last_frame_time >= 0.04)
+	{
+		if (mlx->sprite_frames[mlx->curr_frame])
+			mlx_delete_image(mlx->mlx, mlx->sprite_frames[mlx->curr_frame]);
+		mlx->curr_frame++;
+		if (mlx->curr_frame >= mlx->num_frames)
+		{
+			mlx->curr_frame = 0;
+			mlx->is_animated = 0;
+		}
+		mlx->sprite_frames[mlx->curr_frame] = mlx_texture_to_image(mlx->mlx,
+				mlx->sprite_textures[mlx->curr_frame]);
+		draw_sprite(mlx, mlx->sprite_frames[mlx->curr_frame],
+			mlx->sprite_textures[mlx->curr_frame]);
+		mlx->last_frame_time = current_time;
 	}
 }
 
 void	animate_sprite(my_mlx_t *mlx)
 {
 	static int	last_state;
-	int 		curr_state;
+	int			curr_state;
 	double		current_time;
 
+	current_time = 0.0;
 	last_state = 0;
 	curr_state = mlx_is_mouse_down(mlx->mlx, MLX_MOUSE_BUTTON_RIGHT);
 	if (curr_state && !last_state && !mlx->is_animated)
@@ -77,22 +101,6 @@ void	animate_sprite(my_mlx_t *mlx)
 		mlx->last_frame_time = mlx_get_time();
 	}
 	last_state = curr_state;
-    if (mlx->is_animated)
-	{
-		current_time = mlx_get_time();
-		if (current_time - mlx->last_frame_time >= 0.04)
-		{
-			if (mlx->sprite_frames[mlx->curr_frame])
-				mlx_delete_image(mlx->mlx, mlx->sprite_frames[mlx->curr_frame]);
-			mlx->curr_frame++;
-			if (mlx->curr_frame >= mlx->num_frames)
-			{
-				mlx->curr_frame = 0;
-				mlx->is_animated = 0;
-			}
-			mlx->sprite_frames[mlx->curr_frame] = mlx_texture_to_image(mlx->mlx, mlx->sprite_textures[mlx->curr_frame]);
-			draw_sprite(mlx, mlx->sprite_frames[mlx->curr_frame], mlx->sprite_textures[mlx->curr_frame]);
-			mlx->last_frame_time = current_time;
-		}
-	}
+	if (mlx->is_animated)
+		animation_process(mlx, current_time);
 }
